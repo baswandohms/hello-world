456add8472c98939c39d928dbae6940bae0d7bd3
baswandohms <35515396+baswandohms@users.noreply.github.com>
2018-01-17 10:57:34 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+var myHeading = document.querySelector('h1');
+var myImage = document.querySelector('img');
+var myButton = document.querySelector('button');
+
+myHeading.textContent = 'Hello world!';
+
+myHeading.onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
